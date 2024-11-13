369619f58ef5d3a083758faa736aa3d8e949cd24
NaomieClareEdward <142856606+NaomieClareEdward@users.noreply.github.com>
2024-11-13 12:55:42 -0500

    enabling CORS

--- HPlusSport.Security.API/Program.cs
+++ HPlusSport.Security.API/Program.cs
@@ -4,7 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "Temperature API Policy",
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:6002");
+        }
+        )
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,12 +24,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors();
 
 app.MapGet("/temperature", () =>
 {
     return new Random().Next(30, 90);
 })
 .WithName("GetTemperature")
-.WithOpenApi();
+.WithOpenApi()
+.RequireCors("Temperature API Policy");
 
 app.Run();
