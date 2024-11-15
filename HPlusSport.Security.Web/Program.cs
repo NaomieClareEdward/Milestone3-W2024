aa99c1401ca40d2032c688466cbec5380d9d930d
NaomieClareEdward <142856606+NaomieClareEdward@users.noreply.github.com>
2024-11-15 15:01:53 -0500

    enforcing https

--- HPlusSport.Security.Web/Program.cs
+++ HPlusSport.Security.Web/Program.cs
@@ -7,14 +7,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHsts(options =>
+{
+    options.MaxAge= TimeSpan.FromDays(365);
+    options.IncludeSubDomains = true;
+})
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
         options.LoginPath = new PathString("/Account/Login");
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.Strict;
     });
 
-builder.Services.AddSession();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.SecurePolicy= CookieSecurePolicy.Always;
+    options.Cookie.SameSite = SameSiteMode.Strict;
+});
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddDbContext<ShopContext>(options =>
